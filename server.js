e7bbc85d8da1fec77de6b928223cc14f67086f0a
saikamara59 <mhsaidu@gmail.com>
2024-11-21 18:09:50 -0500

    late

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware setup
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 app.use(bodyParser.json()); 
 
 // Routes
