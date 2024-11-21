ebbed30b6042816df8f101b081838d8bcea3b322
saikamara59 <mhsaidu@gmail.com>
2024-11-21 18:14:20 -0500

    updated m

--- models/song.js
+++ models/song.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const songSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    artist: { type: mongoose.Schema.Types.ObjectId, ref: 'Artist' },
+    artist: { type:[ mongoose.Schema.Types.ObjectId], ref: 'Artist' },
     genre: { type: String },
     releaseDate: { type: String },
-    image: { type: String},
+    image: { type: String}
 });
 
 
