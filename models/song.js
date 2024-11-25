ea2c3bd186a9e0dd2da7a7dee5a28c6508fd124d
saikamara59 <mhsaidu@gmail.com>
2024-11-25 11:05:15 -0500

    added back

--- models/song.js
+++ models/song.js
@@ -1,17 +1,9 @@
 const mongoose = require('mongoose');
 
-const artistSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    bio: { type: String },
-    genre: { type: String },
-    birthDate: { type: String }
-});
-
 
 const songSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    artist: [ artistSchema],
-    // artist: { type: mongoose.Schema.Types.ObjectId, ref: 'Artist' },
+    artist: { type: mongoose.Schema.Types.ObjectId, ref: 'Artist' },
     genre: { type: String },
     releaseDate: { type: String },
     image: { type: String},
