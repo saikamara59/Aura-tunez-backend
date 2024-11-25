dead0993d215c3d8a542d38564f853110fea241a
saikamara59 <mhsaidu@gmail.com>
2024-11-25 11:00:53 -0500

    testing

--- models/artist.js
+++ models/artist.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const artistSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    bio: { type: String },
-    genre: { type: String },
-    birthDate: { type: String }
-});
+// const artistSchema = new mongoose.Schema({
+//     name: { type: String, required: true },
+//     bio: { type: String },
+//     genre: { type: String },
+//     birthDate: { type: String }
+// });
 
-module.exports = mongoose.model('Artist', artistSchema);
+// module.exports = mongoose.model('Artist', artistSchema);
--- models/song.js
+++ models/song.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
+const artistSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    bio: { type: String },
+    genre: { type: String },
+    birthDate: { type: String }
+});
+
+
 const songSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    artist: { type: mongoose.Schema.Types.ObjectId, ref: 'Artist' },
+    artist: [ artistSchema],
+    // artist: { type: mongoose.Schema.Types.ObjectId, ref: 'Artist' },
     genre: { type: String },
     releaseDate: { type: String },
     image: { type: String},
